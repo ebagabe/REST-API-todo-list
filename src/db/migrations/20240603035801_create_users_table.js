18d7414750eefdc5c109d6f5224ba4afdfdb784e
ebagabe <gabriel.ebagabe@gmail.com>
2024-06-03 00:23:35 -0400

    Corrigindo testes e configuracao

--- src/db/migrations/20240603035801_create_users_table.js
+++ src/db/migrations/20240603035801_create_users_table.js
@@ -8,7 +8,8 @@ exports.up = function(knex) {
     table.string('username').notNullable();
     table.string('email').notNullable().unique();
     table.string('password').notNullable();
-    table.timestamp(true, true);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 };
 
